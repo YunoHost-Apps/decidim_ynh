7ede6cf4cdd4264091188b11a2ebc225a2c1e357
lapineige <lapineige@users.noreply.github.com>
2023-06-25 18:02:03 +0200

    V2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -10,8 +10,8 @@ bundler_version=2.2.19
 rails_version=6.1
 
 # dependencies used by the app (must be on a single line)
-pkg_dependencies="postgresql imagemagick redis-server"
-build_pkg_dependencies="libyaml-dev libreadline6-dev libncurses5-dev libffi-dev libgdbm-dev libpq-dev libicu-dev"
+#REMOVEME? pkg_dependencies="postgresql imagemagick redis-server"
+#REMOVEME? build_pkg_dependencies="libyaml-dev libreadline6-dev libncurses5-dev libffi-dev libgdbm-dev libpq-dev libicu-dev"
 
 #=================================================
 # PERSONAL HELPERS
